4a6ea864c7669ad75997ec5daf2de2dd9a3c2ccc
JV-conseil <contact@jv-conseil.net>
2023-03-20 19:01:07 +0100

    1.1

--- rlink.sh
+++ rlink.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #
 # title         : rlink.sh
-# description   : A shell script to mount TOMTOM.xxx files from a TomTom 
+# description   : A shell script to mount TOMTOM.xxx files from a TomTom
 #                 SD card to browse/modify its content.
 # author        : JV conseil – Internet Consulting
 # credits       : JV-conseil
-# licence       : BSD 3-Clause License.
 # copyright     : Copyright (c) 2021, JV conseil – Internet Consulting,
 #                 All rights reserved.
 # usage         : bash ./rlink.sh
-# requirements  : VirtualBox, VM VirtualBox Extension Pack and one Linux 
+# requirements  : VirtualBox, VM VirtualBox Extension Pack and one Linux
 #                 distribution like Ubuntu.
 # date          : 20210525
 # version       : 1.0
@@ -17,17 +16,15 @@
 #
 #===============================================================================
 
-
 __TOMTOM_PATH="./"
 
-
-__rlink_help () {
-    cat << EOF
+__rlink_help() {
+  cat <<EOF
 
 R-Link Evolution Missing Explorer
 ---------------------------------
 
-Place this file in the same folder where your TOMTOM.000 ... files are, ideally 
+Place this file in the same folder where your TOMTOM.000 ... files are, ideally
 on the SD card:
 
 /Volumes/SDCARD
@@ -52,321 +49,285 @@ Once done, you can modify the content, to add, for example, POIs.
 EOF
 }
 
-
 # Output to stderr
-__rlink_echoerr () {
-    echo "$@" 1>&2;
+__rlink_echoerr() {
+  echo "$@" 1>&2
 }
-
 
 # Find first available /dev/loopX
-__rlink_loopback_dev () {
+__rlink_loopback_dev() {
 
-    typeset -i num
-    num=0
-    while [[ ${num} -le 255 ]]
-    do
-        if ! losetup /dev/loop${num} >/dev/null 2>&1
-        then
-            break
-        fi
-        num=$((num + 2))
-    done
+  typeset -i num
+  num=0
+  while [[ ${num} -le 255 ]]; do
+    if ! losetup /dev/loop${num} >/dev/null 2>&1; then
+      break
+    fi
+    num=$((num + 2))
+  done
 
-    echo "/dev/loop${num}"
+  echo "/dev/loop${num}"
 
 }
-
 
 # Count the number of TOMTOM.xxx files
-__rlink_count_tomtom_vfs_files () {
+__rlink_count_tomtom_vfs_files() {
 
-    typeset -i count_vfs_files
-    count_vfs_files=0
+  typeset -i count_vfs_files
+  count_vfs_files=0
 
-    while [[ -e ${__TOMTOM_PATH}TOMTOM.$(printf "%03d" ${count_vfs_files}) ]]
-    do
-        count_vfs_files+=1
-    done
+  while [[ -e ${__TOMTOM_PATH}TOMTOM.$(printf "%03d" ${count_vfs_files}) ]]; do
+    count_vfs_files+=1
+  done
 
-    echo ${count_vfs_files}
+  echo ${count_vfs_files}
 
 }
-
 
 # Associate all TOMTOM.xxx files to a loopback device
-__rlink_init_loopback_devs () {
+__rlink_init_loopback_devs() {
 
-    typeset -gi COUNT_VFS_FILES
-    LOOPBACK_DEV_LIST=""
+  typeset -gi COUNT_VFS_FILES
+  LOOPBACK_DEV_LIST=""
 
-    COUNT_VFS_FILES=$(__rlink_count_tomtom_vfs_files ${__TOMTOM_PATH})
-    echo "${COUNT_VFS_FILES} tomtom files found."
+  COUNT_VFS_FILES=$(__rlink_count_tomtom_vfs_files ${__TOMTOM_PATH})
+  echo "${COUNT_VFS_FILES} tomtom files found."
 
-    echo "Creating loopback devices."
+  echo "Creating loopback devices."
 
-    for ((i=0;i<COUNT_VFS_FILES;i++))
-    do
-        VFS_FILE=${__TOMTOM_PATH}TOMTOM.$(printf "%03d" "$i")
-        LOOPBACK_DEV=$(__rlink_loopback_dev)
-        LOOPBACK_DEV_LIST="${LOOPBACK_DEV_LIST} ${LOOPBACK_DEV}"
+  for ((i = 0; i < COUNT_VFS_FILES; i++)); do
+    VFS_FILE=${__TOMTOM_PATH}TOMTOM.$(printf "%03d" "$i")
+    LOOPBACK_DEV=$(__rlink_loopback_dev)
+    LOOPBACK_DEV_LIST="${LOOPBACK_DEV_LIST} ${LOOPBACK_DEV}"
 
-        # associate file to loopback device
-        if ! sudo losetup "${LOOPBACK_DEV}" "${VFS_FILE}"
-        then
-            __rlink_echoerr "Error while creating loopback device ${LOOPBACK_DEV} from ${VFS_FILE}. Cancel."
-            __rlink_remove_loopback_devs
-            exit 1
-        fi
-        
-        echo "${VFS_FILE} is now associated to ${LOOPBACK_DEV}."
-    done
+    # associate file to loopback device
+    if ! sudo losetup "${LOOPBACK_DEV}" "${VFS_FILE}"; then
+      __rlink_echoerr "Error while creating loopback device ${LOOPBACK_DEV} from ${VFS_FILE}. Cancel."
+      __rlink_remove_loopback_devs
+      exit 1
+    fi
+
+    echo "${VFS_FILE} is now associated to ${LOOPBACK_DEV}."
+  done
 
 }
-
 
 # Delete all loopback devices
-__rlink_remove_loopback_devs () {
+__rlink_remove_loopback_devs() {
 
-    echo "Removing all loopback devices."
-    # shellcheck disable=SC2086
-    if ! sudo losetup -d $LOOPBACK_DEV_LIST
-    then
-        __rlink_echoerr "Error while removing one or multiple loopback devices. Please check with losetp and dmesg."
-        exit 1
-    fi
-    return 0
+  echo "Removing all loopback devices."
+  # shellcheck disable=SC2086
+  if ! sudo losetup -d $LOOPBACK_DEV_LIST; then
+    __rlink_echoerr "Error while removing one or multiple loopback devices. Please check with losetp and dmesg."
+    exit 1
+  fi
+  return 0
 
 }
-
 
 # Build the linear raid device from all loopback devices (TOMTOM.xxx files)
-__rlink_build_linear_raid () {
+__rlink_build_linear_raid() {
 
-    echo "Build linear raid device."
-    COMMAND="mdadm --build --auto=part --verbose /dev/md/tomtom_vfs --rounding=32 --level=linear -n${COUNT_VFS_FILES} ${LOOPBACK_DEV_LIST}"
-    # shellcheck disable=SC2086
-    if ! sudo $COMMAND
-    then
-        __rlink_echoerr "Error during raid device creation. Trying to cancel. Check /proc/mdstat, mdadm and dmesg."
-        __rlink_remove_loopback_devs
-        exit 1
-    fi
-    return 0
+  echo "Build linear raid device."
+  COMMAND="mdadm --build --auto=part --verbose /dev/md/tomtom_vfs --rounding=32 --level=linear -n${COUNT_VFS_FILES} ${LOOPBACK_DEV_LIST}"
+  # shellcheck disable=SC2086
+  if ! sudo $COMMAND; then
+    __rlink_echoerr "Error during raid device creation. Trying to cancel. Check /proc/mdstat, mdadm and dmesg."
+    __rlink_remove_loopback_devs
+    exit 1
+  fi
+  return 0
 
 }
-
 
 # Delete the /dev/md/tomtom_vfs raid device
-__rlink_delete_linear_raid () {
+__rlink_delete_linear_raid() {
 
-    echo "Stopping linear raid device."
-    if ! sudo mdadm -S /dev/md/tomtom_vfs
-    then
-        __rlink_echoerr "Error during raid device deletion. Please check /proc/mdstat, mdadm and dmesg. Loopback devices won't be removed."
-        exit 1
-    fi
-    return 0
+  echo "Stopping linear raid device."
+  if ! sudo mdadm -S /dev/md/tomtom_vfs; then
+    __rlink_echoerr "Error during raid device deletion. Please check /proc/mdstat, mdadm and dmesg. Loopback devices won't be removed."
+    exit 1
+  fi
+  return 0
 
 }
-
 
 # wait for a carriage return
-__press_enter_to_continue () {
+__press_enter_to_continue() {
 
-    # shellcheck disable=SC2162
-    read -p "Please press enter to continue:"
-    echo "Thank you."
-    return 0
+  # shellcheck disable=SC2162
+  read -p "Please press enter to continue:"
+  echo "Thank you."
+  return 0
 
 }
-
 
 # Mount the TOMTOM.xxx linear raid ex3 filesystem in /mnt/tomtom_vfs
-__rlink_mount_vfs () {
+__rlink_mount_vfs() {
 
-    # Ensure it's not already mounted
-    mount | grep "on /mnt/tomtom_vfs " >/dev/null 2>&1
-    # shellcheck disable=SC2181
-    if [[ $? -eq 0 ]]
-    then
-        echo "Mountpoint already in use. Cancel."
+  # Ensure it's not already mounted
+  mount | grep "on /mnt/tomtom_vfs " >/dev/null 2>&1
+  # shellcheck disable=SC2181
+  if [[ $? -eq 0 ]]; then
+    echo "Mountpoint already in use. Cancel."
+  else
+    echo "Creating mount point and mount tomtom filesystem."
+    sudo mkdir -p /mnt/tomtom_vfs 2>/dev/null
+    sudo mount /dev/md/tomtom_vfs /mnt/tomtom_vfs
+    if [[ $? -eq 0 ]]; then
+      vfs_user=$(stat -c "%u" /mnt/tomtom_vfs/common)
+      vfs_group=$(stat -c "%g" /mnt/tomtom_vfs/common)
+      current_group=$(id -gn)
+
+      # bind to
+      mkdir -p "$HOME/tomtom_vfs"
+      sudo bindfs -u "$USER" -g "${current_group}" --create-for-user="${vfs_user}" --create-for-group="${vfs_group}" /mnt/tomtom_vfs/ "$HOME/tomtom_vfs"
+      echo "Filesystem is now available in /mnt/tomtom_vfs as root and $HOME/tomtom_vfs as $USER."
+      sleep 1
     else
-        echo "Creating mount point and mount tomtom filesystem."
-        sudo mkdir -p /mnt/tomtom_vfs 2>/dev/null
-        sudo mount /dev/md/tomtom_vfs /mnt/tomtom_vfs
-        if [[ $? -eq 0 ]]
-        then 
-            vfs_user=$(stat -c "%u" /mnt/tomtom_vfs/common)
-            vfs_group=$(stat -c "%g" /mnt/tomtom_vfs/common)
-            current_group=$(id -gn)
-
-            # bind to 
-            mkdir -p "$HOME/tomtom_vfs"
-            sudo bindfs -u "$USER" -g "${current_group}" --create-for-user="${vfs_user}" --create-for-group="${vfs_group}" /mnt/tomtom_vfs/ "$HOME/tomtom_vfs"
-            echo "Filesystem is now available in /mnt/tomtom_vfs as root and $HOME/tomtom_vfs as $USER."
-            sleep 1
-        else
-            __rlink_echoerr "Something is wrong. Corrupted files ? Trying to cancel."
-            __rlink_umount_vfs
-            __rlink_delete_linear_raid
-            __rlink_remove_loopback_devs
-            exit 1
-        fi     
+      __rlink_echoerr "Something is wrong. Corrupted files ? Trying to cancel."
+      __rlink_umount_vfs
+      __rlink_delete_linear_raid
+      __rlink_remove_loopback_devs
+      exit 1
     fi
-    return 0
+  fi
+  return 0
 }
-
 
 # unmount /mnt/tomtom_vfs (TOMTOM.xxx linear raid ext3 filesystem)
-__rlink_umount_vfs () {
+__rlink_umount_vfs() {
 
-    # Ensure it's mounted
-    mount | grep "on /mnt/tomtom_vfs " >/dev/null 2>&1
-    # shellcheck disable=SC2181
-    if [[ $? -ne 0 ]]
-    then
-        echo "Nothing to do, /mnt/tomtom_vfs Not mounted."
-        return 0
-    fi
-
-    # synchronise files
-    sync
-
-    # check if there's something to kill
-    fuserkill=0
-    lsof "$HOME/tomtom_vfs" >/dev/null 2>&1
-    [[ $? -ne 1 ]] && fuserkill=1
-    lsof /mnt/tomtom_vfs >/dev/null 2>&1
-    [[ $? -ne 1 ]] && fuserkill=1
-
-    if [[ ${fuserkill} -eq 1 ]]
-    then
-        # Killing local users
-        echo "Killing remaining processus using the mountpoints."
-        sudo fuser -k "$HOME/tomtom_vfs"
-
-        # First kill everything that is related to the mountpoint
-        sudo fuser -k /mnt/tomtom_vfs
-
-        echo "Waiting 5 seconds."
-        sleep 5
-    fi
-
-    # Then, umount
-    echo "Unmounting tomtom filesystem and remove mount point."
-    sudo fusermount -u "$HOME/tomtom_vfs"
-    if ! sudo umount /mnt/tomtom_vfs
-    then
-        __rlink_echoerr "Cannot unmount /mnt/tomtom_vfs. Please check dmesg, fuser and dmesg. raid and loopback devices left untouched."
-        exit 1
-    fi
-    sudo rmdir /mnt/tomtom_vfs
-    sudo rmdir "$HOME/tomtom_vfs"
+  # Ensure it's mounted
+  mount | grep "on /mnt/tomtom_vfs " >/dev/null 2>&1
+  # shellcheck disable=SC2181
+  if [[ $? -ne 0 ]]; then
+    echo "Nothing to do, /mnt/tomtom_vfs Not mounted."
     return 0
+  fi
+
+  # synchronise files
+  sync
+
+  # check if there's something to kill
+  fuserkill=0
+  lsof "$HOME/tomtom_vfs" >/dev/null 2>&1
+  [[ $? -ne 1 ]] && fuserkill=1
+  lsof /mnt/tomtom_vfs >/dev/null 2>&1
+  [[ $? -ne 1 ]] && fuserkill=1
+
+  if [[ ${fuserkill} -eq 1 ]]; then
+    # Killing local users
+    echo "Killing remaining processus using the mountpoints."
+    sudo fuser -k "$HOME/tomtom_vfs"
+
+    # First kill everything that is related to the mountpoint
+    sudo fuser -k /mnt/tomtom_vfs
+
+    echo "Waiting 5 seconds."
+    sleep 5
+  fi
+
+  # Then, umount
+  echo "Unmounting tomtom filesystem and remove mount point."
+  sudo fusermount -u "$HOME/tomtom_vfs"
+  if ! sudo umount /mnt/tomtom_vfs; then
+    __rlink_echoerr "Cannot unmount /mnt/tomtom_vfs. Please check dmesg, fuser and dmesg. raid and loopback devices left untouched."
+    exit 1
+  fi
+  sudo rmdir /mnt/tomtom_vfs
+  sudo rmdir "$HOME/tomtom_vfs"
+  return 0
 
 }
-
 
 # If on X, open default file manager
-__rlink_open_default_file_manager () {
+__rlink_open_default_file_manager() {
 
-    if xhost >& /dev/null
-    then 
-        echo "Launching default file manager."
-        xdg-open "$HOME/tomtom_vfs"
-    fi
-    return 0
+  if xhost >&/dev/null; then
+    echo "Launching default file manager."
+    xdg-open "$HOME/tomtom_vfs"
+  fi
+  return 0
 
 }
-
 
 # Check if prerequires are installed
-__rlink_check_prerequires () {
+__rlink_check_prerequires() {
 
-    ERR=0
-    PREREQUIRES="mdadm losetup bindfs"
-    for prerequire in $PREREQUIRES
-    do
-        if ! which "$prerequire" >/dev/null 2>&1
-        then
-            __rlink_echoerr "$prerequire command is missing."
-            echo "Please install it (the method depends of your Linux distribution)."
-            echo "For Ubuntu: apt-get install $prerequire"
-            ERR=1
-        fi
-    done
-    return $ERR
-
-}
-
-
-__rlink_search_cards () {
-
-    typeset -i COUNT_CARDS
-    COUNT_CARDS=0
-
-    # shellcheck disable=SC2044
-    for card in $(find "$HOME/tomtom_vfs" -name "*.pna")
-    do
-        echo "Found $card card."
-        COUNT_CARDS+=1
-    done
-
-    echo "Found ${COUNT_CARDS} card(s)."
-    return 0
-
-}
-
-
-rlink () {
-
-    if ! __rlink_check_prerequires
-    then
-        exit 1
+  ERR=0
+  PREREQUIRES="mdadm losetup bindfs"
+  for prerequire in $PREREQUIRES; do
+    if ! which "$prerequire" >/dev/null 2>&1; then
+      __rlink_echoerr "$prerequire command is missing."
+      echo "Please install it (the method depends of your Linux distribution)."
+      echo "For Ubuntu: apt-get install $prerequire"
+      ERR=1
     fi
-
-    echo "Starting."
-
-    __rlink_init_loopback_devs
-    __rlink_build_linear_raid
-    __rlink_mount_vfs
-    __rlink_search_cards
-    __rlink_open_default_file_manager
-    __press_enter_to_continue
-    __rlink_umount_vfs
-    __rlink_delete_linear_raid
-    __rlink_remove_loopback_devs
-
-    echo "End."
-    return 0
+  done
+  return $ERR
 
 }
 
+__rlink_search_cards() {
+
+  typeset -i COUNT_CARDS
+  COUNT_CARDS=0
+
+  # shellcheck disable=SC2044
+  for card in $(find "$HOME/tomtom_vfs" -name "*.pna"); do
+    echo "Found $card card."
+    COUNT_CARDS+=1
+  done
+
+  echo "Found ${COUNT_CARDS} card(s)."
+  return 0
+
+}
+
+rlink() {
+
+  if ! __rlink_check_prerequires; then
+    exit 1
+  fi
+
+  echo "Starting."
+
+  __rlink_init_loopback_devs
+  __rlink_build_linear_raid
+  __rlink_mount_vfs
+  __rlink_search_cards
+  __rlink_open_default_file_manager
+  __press_enter_to_continue
+  __rlink_umount_vfs
+  __rlink_delete_linear_raid
+  __rlink_remove_loopback_devs
+
+  echo "End."
+  return 0
+
+}
 
 # Tree
-if [[ -z "$1" ]]
-then
-    __rlink_help
-    echo
-    read -p "Do you want to assemble your TOMTOM files? [y/N] " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]
-    then
-        rlink "$@"
-    fi
+if [[ -z "$1" ]]; then
+  __rlink_help
+  echo
+  read -p "Do you want to assemble your TOMTOM files? [y/N] " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    rlink "$@"
+  fi
 else
-    while getopts ":h" option
-            do
-            case $option in
-                h) # display Help
-                    __rlink_help
-                    ;;
+  while getopts ":h" option; do
+    case $option in
+    h) # display Help
+      __rlink_help
+      ;;
 
-                \?) # incorrect option
-                    __rlink_echoerr "Error: Invalid option."
-                    __rlink_help
-                    ;;
-            esac
-            done
+    \?) # incorrect option
+      __rlink_echoerr "Error: Invalid option."
+      __rlink_help
+      ;;
+    esac
+  done
 fi
--- virtualbox.sh
+++ virtualbox.sh
@@ -4,11 +4,10 @@
 # description   : VirtualBox v6.1 Extra Ressources.
 # author        : JV conseil – Internet Consulting
 # credits       : JV-conseil
-# licence       : BSD 3-Clause License.
 # copyright     : Copyright (c) 2021, JV conseil – Internet Consulting,
 #                 All rights reserved.
 # usage         : vbox
-# requirements  : VirtualBox, VM VirtualBox Extension Pack and one Linux 
+# requirements  : VirtualBox, VM VirtualBox Extension Pack and one Linux
 #                 distribution like Ubuntu.
 # date          : 20210525
 # version       : 1.0
@@ -18,9 +17,8 @@
 
 __VIRTUALBOX_PATH="$HOME/VirtualBox/Ubuntu"
 
-
-__virtualbox_help () {
-    cat << EOF
+__virtualbox_help() {
+  cat <<EOF
 
 VirtualBox v6.1 Extra Ressources
 --------------------------------
@@ -31,8 +29,8 @@ https://www.virtualbox.org
 
 Download Ubuntu Desktop
 Ubuntu 20.04.2.0 LTS
-LTS stands for long-term support — which means 
-five years, until April 2025, of free security 
+LTS stands for long-term support — which means
+five years, until April 2025, of free security
 and maintenance updates, guaranteed:
 https://ubuntu.com/download/desktop
 
@@ -51,162 +49,151 @@ ${__VIRTUALBOX_PATH}
 EOF
 }
 
-
 # wait for a carriage return
-__press_enter_to_continue () {
+__press_enter_to_continue() {
 
-    # echo "Please press return to finish :"
-    # read -r dummy
-    # shellcheck disable=SC2162
-    read -p "Please press enter to continue:"
-    echo "Thank you."
-    return 0
-
-}
-
-
-__virtualbox_brew () {
-
-    __brew_pkg=("virtualbox" "virtualbox-extension-pack")
-
-    echo
-    echo "Checking your Homebrew packages list"
-    echo "------------------------------------"
-    for pkg in "${__brew_pkg[@]}"
-    do
-        brew list --cask --versions "$pkg" || brew install "$pkg"
-    done
-    echo
+  # echo "Please press return to finish :"
+  # read -r dummy
+  # shellcheck disable=SC2162
+  read -p "Please press enter to continue:"
+  echo "Thank you."
+  return 0
 
 }
 
+__virtualbox_brew() {
 
-__virtualbox_kill_apps () {
+  __brew_pkg=("virtualbox" "virtualbox-extension-pack")
 
-    echo "Quitting opened apps that should not run during this procedure."
-    pkill -x VirtualBox RLinkToolbox
+  echo
+  echo "Checking your Homebrew packages list"
+  echo "------------------------------------"
+  for pkg in "${__brew_pkg[@]}"; do
+    brew list --cask --versions "$pkg" || brew install "$pkg"
+  done
+  echo
 
 }
 
+__virtualbox_kill_apps() {
 
-__virtualbox_sdcard () {
+  echo "Quitting opened apps that should not run during this procedure."
+  pkill -x VirtualBox RLinkToolbox
 
-    __virtualbox_kill_apps
+}
 
-    cd "${__VIRTUALBOX_PATH}" || exit
+__virtualbox_sdcard() {
 
-    echo
-    echo "Create a Virtual Machine VMDK for your SD card"
-    echo "----------------------------------------------"
+  __virtualbox_kill_apps
 
-    sudo diskutil list
+  cd "${__VIRTUALBOX_PATH}" || exit
 
-    echo
-    read -p "Enter the correct disk number /dev/disk[0-9]: " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[0-9]$ ]]
-    then
-        __disk="/dev/disk${REPLY}"
-        __sdcard="SDcard.vmdk"
-        __unmount () {
-            sudo diskutil unmountDisk "${__disk}"
-        }
+  echo
+  echo "Create a Virtual Machine VMDK for your SD card"
+  echo "----------------------------------------------"
 
-        echo "You have selected: ${__disk}."
-        sudo chown "$USER" "${__disk}"
-        sudo chmod 777 "${__disk}"
+  sudo diskutil list
 
-        if [[ -f "${__sdcard}" ]]
-        then
-            echo "Removing previous VDMK file: ${__sdcard}."
-            rm "${__sdcard}"
-        
-        else
-            echo "No previous ${__sdcard} found to delete."
+  echo
+  read -p "Enter the correct disk number /dev/disk[0-9]: " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[0-9]$ ]]; then
+    __disk="/dev/disk${REPLY}"
+    __sdcard="SDcard.vmdk"
+    __unmount() {
+      sudo diskutil unmountDisk "${__disk}"
+    }
 
-        fi
+    echo "You have selected: ${__disk}."
+    sudo chown "$USER" "${__disk}"
+    sudo chmod 777 "${__disk}"
 
-        __unmount
-
-        sudo VBoxManage internalcommands createrawvmdk -filename "${__sdcard}" -rawdisk "${__disk}"
-        sudo chown "$USER" "${__sdcard}"
-        sudo chmod 777 "${__sdcard}"
-
-        __unmount
-
-        # __uuid=$(sed -En 's/.*?ddb.uuid.image="([a-z0-9\-]+)".*?/\1/' "${__sdcard}")
-        __uuid=$(grep -Eo 'ddb.uuid.image="[a-z0-9\-]+"' "${__sdcard}" | sed -E 's/ddb.uuid.image="([a-z0-9\-]+)"/\1/')
-        if [[ "${__uuid}" ]]
-        then
-            __config_file="Ubuntu.vbox"
-
-            echo "Copying uuid value to clipboard: ${__uuid}."
-            echo "${__uuid}" | tr -d '\n' | pbcopy
-
-            echo "Opening ${__config_file} in TextEdit."
-            open -a TextEdit "${__config_file}"
-
-            echo "In TextEdit look for these two lines:"
-            printf '\t- %s\n' '<HardDisk uuid="{e19b7ecb-3193-4f9b-98ad-d6a0d56ad436}" location="SDcard.vmdk" format="VMDK" type="Normal"/>'
-            printf '\t- %s\n' '<AttachedDevice type="HardDisk" hotpluggable="true" port="1" device="0"><Image uuid="{e19b7ecb-3193-4f9b-98ad-d6a0d56ad436}"/></AttachedDevice>'
-            echo "Replace the uuid value by: ${__uuid}."
-            echo "Save & Quit TextEdit."
-            __unmount
-
-            __press_enter_to_continue
-            open -a VirtualBox
-        fi
+    if [[ -f "${__sdcard}" ]]; then
+      echo "Removing previous VDMK file: ${__sdcard}."
+      rm "${__sdcard}"
 
     else
-        echo "You did not provide a valid number disk. Cancel."
-        exit 1
+      echo "No previous ${__sdcard} found to delete."
+
     fi
+
+    __unmount
+
+    sudo VBoxManage internalcommands createrawvmdk -filename "${__sdcard}" -rawdisk "${__disk}"
+    sudo chown "$USER" "${__sdcard}"
+    sudo chmod 777 "${__sdcard}"
+
+    __unmount
+
+    # __uuid=$(sed -En 's/.*?ddb.uuid.image="([a-z0-9\-]+)".*?/\1/' "${__sdcard}")
+    __uuid=$(grep -Eo 'ddb.uuid.image="[a-z0-9\-]+"' "${__sdcard}" | sed -E 's/ddb.uuid.image="([a-z0-9\-]+)"/\1/')
+    if [[ "${__uuid}" ]]; then
+      __config_file="Ubuntu.vbox"
+
+      echo "Copying uuid value to clipboard: ${__uuid}."
+      echo "${__uuid}" | tr -d '\n' | pbcopy
+
+      echo "Opening ${__config_file} in TextEdit."
+      open -a TextEdit "${__config_file}"
+
+      echo "In TextEdit look for these two lines:"
+      printf '\t- %s\n' '<HardDisk uuid="{e19b7ecb-3193-4f9b-98ad-d6a0d56ad436}" location="SDcard.vmdk" format="VMDK" type="Normal"/>'
+      printf '\t- %s\n' '<AttachedDevice type="HardDisk" hotpluggable="true" port="1" device="0"><Image uuid="{e19b7ecb-3193-4f9b-98ad-d6a0d56ad436}"/></AttachedDevice>'
+      echo "Replace the uuid value by: ${__uuid}."
+      echo "Save & Quit TextEdit."
+      __unmount
+
+      __press_enter_to_continue
+      open -a VirtualBox
+    fi
+
+  else
+    echo "You did not provide a valid number disk. Cancel."
+    exit 1
+  fi
 
 }
 
+vbox() {
 
-vbox () {
+  if [[ -z "$1" ]]; then
+    __virtualbox_help
+    echo
+    read -p "Do you want to create a VMDK for your SD card? [y/N] " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      __virtualbox_sdcard
+    else
+      __virtualbox_help
+    fi
 
-    if [[ -z "$1" ]]
-    then
+  else
+    while getopts ":hcsl" option; do
+      case $option in
+      h) # display Help
         __virtualbox_help
-        echo
-        read -p "Do you want to create a VMDK for your SD card? [y/N] " -n 1 -r
-        echo
-        if [[ $REPLY =~ ^[Yy]$ ]]
-        then
-            __virtualbox_sdcard
-        else
-            __virtualbox_help
-        fi
+        ;;
 
-    else
-        while getopts ":hcsl" option
-        do
-        case $option in
-            h) # display Help
-                __virtualbox_help
-                ;;
+      c)
+        __virtualbox_brew
+        ;;
 
-            c)
-                __virtualbox_brew
-                ;;
+      s)
+        __virtualbox_sdcard
+        ;;
 
-            s)
-                __virtualbox_sdcard
-                ;;
-            
-            l)
-                open "https://myr.renault.fr/r-link-store.html"
-                open -a RLinkToolbox
-                ;;
+      l)
+        open "https://myr.renault.fr/r-link-store.html"
+        open -a RLinkToolbox
+        ;;
 
-            \?) # incorrect option
-                echo "Error: Invalid option."
-                exit 1;;
-        esac
-        done
+      \?) # incorrect option
+        echo "Error: Invalid option."
+        exit 1
+        ;;
+      esac
+    done
 
-    fi
+  fi
 
 }
